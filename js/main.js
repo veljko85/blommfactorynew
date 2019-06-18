1a6543da4f7b69a78bd0da52a128f49decd4077b
Velibor Markovic <veljko85@users.noreply.github.com>
2019-06-18 14:49:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,7 +16,11 @@ var navFooter = document.getElementById("nav-footer");
 var dekoracijaButton = document.getElementById("dekoracija-button");
 var navBackground = document.getElementById("nav-background");
 var logoLanding = document.getElementById("logo-landing");
+var heightScale = document.getElementsByClassName("height-scale");
 
+for (var i = 0; i < heightScale.length; i++) {
+	heightScale[i].style.height = window.innerHeight - 62 + "px";
+}
 
 
 if (document.body.clientWidth < 768){
@@ -48,7 +52,6 @@ proslaveShow.onclick = () => {
 	}
 }
 
-console.log(navButtons);
 
 hamBut.onclick = () => {
 
@@ -64,7 +67,7 @@ hamBut.onclick = () => {
 			dekoracijaButton.classList.remove("show-footer");
 			navFooter.children[0].classList.remove("show-footer");
 			navFooter.children[1].classList.remove("show-footer");
-			navBackground.style.display = "none";
+			navBackground.style.opacity = "0.2";
 
 		}	else	{
 			firLin.classList.add("change1");
@@ -83,7 +86,7 @@ hamBut.onclick = () => {
 			dekoracijaButton.classList.add("show-footer");
 			navFooter.children[0].classList.add("show-footer");
 			navFooter.children[1].classList.add("show-footer");
-			navBackground.style.display = "block";
+			navBackground.style.opacity = "0.7";
 		}
 
 		if (document.body.clientWidth < 768){
